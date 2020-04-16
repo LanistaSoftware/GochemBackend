65c396a0408c2d3e8dea42ad905f25104809d365
serkankorkac <korkacserkan@gmail.com>
2020-04-16 14:34:07 +0300

    multer url düzenlendi

--- server/api/middleware/multer.js
+++ server/api/middleware/multer.js
@@ -1,9 +1,8 @@
 const multer  = require('multer');
 const path = require('path')
-
 const diskStorageToUploads = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null,"https://test.lanista.com.tr/img")
+        cb(null,path.join(__dirname, "../../../assest/images"))
     },
     filename: (req, file, cb) => {
         cb(null, file.originalname+ '-' + new Date().getUTCMonth() + '-' + new Date().getUTCDay() + '-'+'.jpg')
