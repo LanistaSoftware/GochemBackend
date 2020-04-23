daeeaecb082df020d25fece869b9718f2401b0d4
serkankorkac <korkacserkan@gmail.com>
2020-04-23 17:34:29 +0300

    search düzenlendi

--- server/api/controller/searchController.js
+++ server/api/controller/searchController.js
@@ -4,6 +4,7 @@ const pigment = require('../models/pigmentsSchema');
 const product = require('../models/productSchema')
 const sector = require('../models/sectorSchema')
 const blog = require('../models/makaleschema')
+const document = require('../models/documentSchema')
 
 router.post('/', async (req,res)=>{
     try {
@@ -11,7 +12,8 @@ router.post('/', async (req,res)=>{
     const proddetail = await product.find({ prdoudctName: { $regex:req.body.searchitem, $options: "i" } })
     const sectordetail = await sector.find({ sectorname: { $regex:req.body.searchitem, $options: "i" } })
     const blogdetail= await blog.find({content:{$regex:req.body.searchitem, $options: "i" }})
-	res.status(200).json({pigmentdetail,proddetail,sectordetail,blogdetail}); 
+    const documentdetail= await document.find({name:{$regex:req.body.searchitem, $options: "i" }})
+	res.status(200).json({pigmentdetail,proddetail,sectordetail,blogdetail,documentdetail}); 
     } catch (error) {
         console.log(error)
     }
